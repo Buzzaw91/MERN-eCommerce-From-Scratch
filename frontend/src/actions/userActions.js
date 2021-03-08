95ce59c21e3a2f95406ec84fc96f46818ee7dd21
Buzzaw91 <aleksi.ylonen2@gmail.com>
2021-03-08 14:09:35 +0200

    feat: edit user functionality

--- frontend/src/actions/userActions.js
+++ frontend/src/actions/userActions.js
@@ -20,7 +20,10 @@ import { USER_DETAILS_FAIL,
         USER_LIST_RESET,
         USER_DELETE_REQUEST,
         USER_DELETE_SUCCESS,
-        USER_DELETE_FAIL
+        USER_DELETE_FAIL,
+        USER_UPDATE_REQUEST,
+        USER_UPDATE_SUCCESS,
+        USER_UPDATE_FAIL
         } from '../constants/userConstants';
 
 export const login = (email, password) => async (dispatch) => {
@@ -222,6 +225,34 @@ export const deleteUser = (id) => async (dispatch, getState) => {
     } catch (error) {
         dispatch({
             type: USER_DELETE_FAIL,
+            payload: error.response && error.response.data.message ?
+            error.response.data.message : error.message
+        });
+    }
+}
+
+export const updateUser = (user) => async (dispatch, getState) => {
+    try {
+        dispatch({
+            type: USER_UPDATE_REQUEST
+        });
+
+        const { userLogin: { userInfo } } = getState();
+
+        const config = {
+            headers: {
+                'Content-Type': 'application/json',
+                authorization: `Bearer ${userInfo.token}`
+            },
+        }
+
+        const { data } = await axios.put(`/api/users/${user._id}`, user, config);
+
+            dispatch({ type: USER_UPDATE_SUCCESS, payload: data });
+
+    } catch (error) {
+        dispatch({
+            type: USER_UPDATE_FAIL,
             payload: error.response && error.response.data.message ?
             error.response.data.message : error.message
         });
