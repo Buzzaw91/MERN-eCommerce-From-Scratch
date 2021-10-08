3c223a330dc8232b2fd6bdbe3bb6ef86fc2351fa
Buzzaw91 <aleksi.ylonen2@gmail.com>
2021-10-08 14:25:43 +0300

    feat: getOrder controller & routes
    Ability to get all orders from the db, route protected and admin only

--- backend/middleware/authMiddleware.js
+++ backend/middleware/authMiddleware.js
@@ -9,9 +9,7 @@ const protect = asyncHandler(async (req, res, next) => {
         try {
             token = req.headers.authorization.split(' ')[1];
 
-            const decoded = jwt.verify(token, process.env.JWT_SECRET,{
-                algorithms: ['HS256']
-            });
+            const decoded = jwt.verify(token, process.env.JWT_SECRET);
 
             req.user = await User.findById(decoded.id).select('-password');
 
